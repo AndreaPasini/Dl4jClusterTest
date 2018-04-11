31f8999aa44bf7bdf933926f3e51ed4a385823d5
Andrea-Dell <andrea.pasini@polito.it>
2018-04-11 16:41:51 +0200

    Dataset creation: read labels and images. Create DataSet object.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,25 +3,34 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.LocatedFileStatus;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.util.hash.Hash;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.input.PortableDataStream;
 import org.apache.spark.partial.PartialResult;
+
+
 import org.apache.spark.sql.SparkSession;
 import org.datavec.image.loader.ImageLoader;
+import org.deeplearning4j.datasets.iterator.impl.CifarDataSetIterator;
+import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator;
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.cpu.nativecpu.NDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.factory.Nd4j;
 import scala.Tuple2;
 
 //import org.slf4j.Logger;
 //import org.slf4j.LoggerFactory;
 
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.IOException;
+import javax.xml.crypto.Data;
+import java.io.*;
 import java.net.URI;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Andrea Pasini
@@ -34,7 +43,9 @@ public class Main {
     //private static Logger log = LoggerFactory.getLogger(Main.class);
 
     //Select between local and remote master
-    public static boolean runLocal = false;
+    public static boolean runLocal = true;
+
+
 
     public static void main(String[] args) throws IOException {
 
@@ -47,35 +58,81 @@ public class Main {
         JavaSparkContext sc = new JavaSparkContext(ss.sparkContext());
         System.out.println("testprint");
 
+        //Reading Class Labels
+        Map<String, INDArray> labels;
+        if (runLocal)
+            labels = DatasetBuilder.readClassLabels(sc,"./data/cifar/labels.txt");
+        else
+            labels = DatasetBuilder.readClassLabels(sc,"hdfs://BigDataHA/user/pasini/data/cifar/labels.txt");
 
+        for (Map.Entry<String, INDArray> e : labels.entrySet()){
+            System.out.println(e.getKey()+" "+e.getValue());
+        }
+
+
+        //Reading training set images
         DirectoryIterator di = new DirectoryIterator();
         if (runLocal)
             di.initIterator("./data/cifar/train/",sc);
         else
             di.initIterator("hdfs://BigDataHA/user/pasini/data/cifar/train/",sc);
-
-
         Configuration conf = sc.hadoopConfiguration();
         FileSystem fs = org.apache.hadoop.fs.FileSystem.get(conf);
 
+        List<INDArray> dsImages = new LinkedList<>();
+        List<INDArray> dsLabels = new LinkedList<>();
+
+        List<DataSet> images=new LinkedList<>();
         int i=0;
-        List<INDArray> images=new LinkedList<>();
-        //List<String> fnames=new LinkedList<>();
+        ImageLoader imageLoader = new ImageLoader();
         while (di.hdfsIterator.hasNext()){
             LocatedFileStatus file = di.hdfsIterator.next();
-
             Path path = file.getPath();
 
-            DataInputStream dis = fs.open(path);
+            //Get image label
+            String label = path.getName().split("_")[1].split("\\.")[0];
+            INDArray labelVect = labels.get(label);
 
-            ImageLoader il = new ImageLoader();
-            INDArray img = il.asMatrix(dis);
-            images.add(img);
+            //Image vectorization
+            DataInputStream dis = fs.open(path);
+            INDArray img = imageLoader.asMatrix(dis);
             dis.close();
+
+            //Add image and label to list
+            dsImages.add(img);
+            dsLabels.add(labelVect);
+
+            if (i>5)break;
             i++;
         }
-        JavaRDD<INDArray> imgRdd = sc.parallelize(images);
-        long num = imgRdd.count();
+
+        //Generate DataSet
+        int[] featureShape = dsImages.get(0).shape();
+        int[] labelShape = dsLabels.get(0).shape();
+        DataSet imageDataset = new DataSet(Nd4j.create(dsImages,new int[]{dsImages.size(),featureShape[0],featureShape[1]}),
+                Nd4j.create(dsLabels, new int[]{dsLabels.size(), labelShape[1]}));
+
+
+        System.out.println("Features: " + imageDataset.getFeatures().shape()[0]+" "+imageDataset.getFeatures().shape()[1]+" "+imageDataset.getFeatures().shape()[2]+" ");
+        System.out.println("Labels: " + imageDataset.getLabels().shape()[0]+" "+imageDataset.getLabels().shape()[1]);
+
+
+
+
+
+
+        //JavaRDD<INDArray> imgRdd = sc.parallelize(images);
+        //long num = imgRdd.count();
+
+
+        //CifarDataSetIterator
+
+
+
+
+
+
+
 
 
         //Reading dataset
@@ -174,4 +231,5 @@ public class Main {
         //log.info("Spark Session closed.");
       //  System.exit(0);
     }
+
 }
